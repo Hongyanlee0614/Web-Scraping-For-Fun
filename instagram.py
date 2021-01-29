f2e977a1be54b43aecc5cfb91a7e2a75d449598b
Hong Yan <hongyanlee0614@gmail.com>
2021-01-29 14:05:50 +0800

    Add files via upload

---
+++ instagram.py
@@ -0,0 +1,21 @@
+import instaloader
+
+# Create an instance of Instaloader class
+bot = instaloader.Instaloader()
+
+# Load a profile from an Instagram handle
+profile = instaloader.Profile.from_username(bot.context, 'yourigusername')
+
+print("Username: ", profile.username)
+print("User ID: ", profile.userid)
+print("Number of Posts: ", profile.mediacount)
+print("Followers: ", profile.followers)
+print("Followees: ", profile.followees)
+print("Bio: ", profile.biography,profile.external_url)
+
+# Retrieve the usernames of all followers
+followers = [follower.username for follower in profile.get_followers()]
+
+# Retrieve the usernames of all followees
+followees = [followee.username for followee in profile.get_followees()]
+print(followers)
